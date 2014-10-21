4bcedfafacf1439c4cd3e08e4d44e18ef4ca8d88
neelay.junnakrar <neelay.junnarkar@gmail.com>
2014-10-21 10:11:24 -0700

    sadfsadfsadf

--- Excercises/src/ch15/exercise5/Main.java
+++ Excercises/src/ch15/exercise5/Main.java
@@ -1,22 +1,48 @@
 package ch15.exercise5;
 
+/*
+ * a midterm:
+ * appendix of thinkapjava on graphics : readdddd
+ * loop invariants
+ * NO CUSTOM IDE COLORING 
+ * dot-product
+ * access modifiers, why, stuff
+ * classes, constructors, inheritance
+ * KNOW toString method
+ * indexing arrays
+ * generalize from a collection of data types to a collection of data
+ * 
+ * 
+ * author: Neelay Junnarkar
+ * 
+ * For 5 cards in a hand:
+ * With nreps set to 1000000, 
+ * 		%flushes is about 0.196%
+ * 		%threes is about 2.22%
+ * 
+ * The Wikipedia Article has probabilities of:
+ * 		%flushes: 0.196%
+ * 		%threes: 2.11%
+ * 
+ * This program has repeatedly outputted numbers such as those stated above with a five card hand, 
+ * with the %flushes being very close to the value wikipedia predicts, ranging from around .193% to .202%, and
+ * the %threes being about .10% to .15% off 
+ * 
+ * For a 7 card hand:
+ * with nreps set to 1000000:
+ * 		%flushes: 0.187%
+ * 		%threes: 2.42%
+ * 
+ */
 public class Main {
+	
 	public static void main(String[] args) {
 		Deck deck = new Deck();
 
 		deck.shuffle();
-//		PokerHand pokerhand = new PokerHand(deck.deal());
-//		pokerhand.print();
-//
-//		if (pokerhand.hasFlush()) {
-//			System.out.println("flushed");
-//		}
-//		if (pokerhand.hasThreeOfKind()) {
-//			System.out.println("three of kind");
-//		}
-		int nreps = 2000;
-		int nflushes = 0;
-		int nthrees = 0;
+		int nreps = 1000000;
+		double nflushes = 0;
+		double nthrees = 0;
 		for (int i = 0; i < nreps; i++) {
 			deck.shuffle();
 			PokerHand hand = new PokerHand(deck.deal());
@@ -27,5 +53,8 @@ public class Main {
 				nthrees++;
 			}
 		}
+		System.out.println("%flushes: "+nflushes/nreps*100);
+		System.out.println("%threes: "+nthrees/nreps*100);
 	}
+	
 }
--- Excercises/src/ch15/exercise5/PokerHand.java
+++ Excercises/src/ch15/exercise5/PokerHand.java
@@ -2,8 +2,10 @@ package ch15.exercise5;
 
 public class PokerHand extends Deck {
 
+	static int ncards = 5; //to change number of cards in a pokerhand, change this value;
+	
 	public PokerHand(Card[] cards) {
-		super(5);
+		super(ncards);
 		this.cards = cards;
 	}
 	
@@ -17,9 +19,35 @@ public class PokerHand extends Deck {
 			if (card.suit != fsuit) {
 				return false;
 			}
-			fsuit = card.suit;
+			//fsuit = card.suit;
 		}
 		return true;	
+	}
+	
+	boolean hasStraightFlush() {
+		return (allSameSuit() && inSeq()) ? true : false;
+	}
+	
+	boolean allSameSuit() {
+		int fsuit = cards[0].suit;
+		for (Card card : cards) {
+			if (card.suit != fsuit)
+				return false;
+		}
+		return true;
+	}
+	
+	boolean inSeq() {
+		int frank = cards[0].rank;
+		String dir;
+		if (cards[1].rank == frank+1) {
+			dir = "up";
+		} else if (cards[1].rank == frank-1) {
+			dir = "down";
+		} else {
+			return false;
+		}
+
 	}
 	
 	boolean hasThreeOfKind() {
@@ -32,6 +60,22 @@ public class PokerHand extends Deck {
 				}
 			}
 			if (n == 3) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	boolean hasFourOfKind() {
+		int n = 0;
+		for (int frank = 0; frank < Card.ranks.length; frank++) {
+			n = 0;
+			for (Card c : cards) {
+				if (c.rank == frank) {
+					n++;
+				}
+			}
+			if (n == 4) {
 				return true;
 			}
 		}
