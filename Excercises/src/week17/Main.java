3a2597ba250e9e8944880a4630b442d4a07e9f3b
neelay.junnakrar <neelay.junnarkar@gmail.com>
2015-01-08 10:15:05 -0800

    priority queue

--- Excercises/src/week17/Main.java
+++ Excercises/src/week17/Main.java
@@ -1,8 +1,18 @@
 package week17;
 
+
 public class Main {
 
 	public static void main(String[] args) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
 		
+		RandP randP = new RandP(0, 100);
+
+		for (int i = 0; i < randP.init_size(); i++) {
+			queue.add(randP.nextInt());
+		}
+
+		queue.bst.printTree();
 	}
+	
 }
