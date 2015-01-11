4ac13c35c2782513eb86b4c2782a0e97f88fe320
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2015-01-10 19:14:58 -0800

    euler

---
+++ Excercises/src/proj_euler/Prob_9.java
@@ -0,0 +1,22 @@
+package proj_euler;
+
+/**
+ * Created by NEELAY on 12/13/2014.
+ */
+public class Prob_9 {
+
+    public static long exec_9() {
+        long n = 1;
+        while (true) {
+
+            if (n*c > 1000)
+                break;
+            n++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exec_9());
+    }
+}
