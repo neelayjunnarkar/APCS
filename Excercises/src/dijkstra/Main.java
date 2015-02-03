de171cfd451d1399d18b1da30ec96a35b6efe324
neelay.junnakrar <neelay.junnarkar@gmail.com>
2015-02-03 12:04:41 -0800

    began

---
+++ Excercises/src/dijkstra/Main.java
@@ -0,0 +1,16 @@
+package dijkstra;
+
+import java.util.HashMap;
+
+/**
+ * This program implements Dijkstra's algorithm in Java
+ *  
+ */
+public class Main {
+    
+    static HashMap<String, HashMap<Node, Integer>> nodes = new HashMap<>();
+    
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Excercises/src/dijkstra/Node.java
@@ -0,0 +1,8 @@
+package dijkstra;
+
+/**
+ * Created by 95028672 on 2/3/2015.
+ */
+public class Node {
+
+}
