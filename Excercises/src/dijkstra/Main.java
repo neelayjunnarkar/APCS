e9e8dd9b378479778425c0b2eb46d41d75e6a152
neelay.junnakrar <neelay.junnarkar@gmail.com>
2015-02-05 09:55:14 -0800

    finds least number of nodes to dest instead of least dist to dest

--- Excercises/src/dijkstra/Main.java
+++ Excercises/src/dijkstra/Main.java
@@ -1,6 +1,7 @@
 package dijkstra;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 
 /**
  * This program implements Dijkstra's algorithm in Java
@@ -8,9 +9,58 @@ import java.util.HashMap;
  */
 public class Main {
     
-    static HashMap<String, HashMap<Node, Integer>> nodes = new HashMap<>();
+    static String[] dijkstra(HashMap<String, Node> graph, String source, String destination) {
+        if (source.equals(destination))
+            return new String[]{"A"};
+
+        int dist = Integer.MAX_VALUE;
+        String node = null;
+
+        if (graph.get(source).parent != null)
+            graph.get(source).dist = graph.get(graph.get(source).parent).dist + graph.get(graph.get(source).parent).getDistTo(source);
+
+        for (String adjacent : graph.get(source).adjacents.keySet()) {
+
+            graph.get(adjacent).parent = source;
+
+            if (graph.get(source).adjacents.get(adjacent) < dist) {
+                dist = graph.get(source).adjacents.get(adjacent);
+                node = adjacent;
+            }
+
+            if (graph.get(source).getDistTo(adjacent)+graph.get(graph.get(adjacent).parent).dist < graph.get(adjacent).dist)
+                graph.get(adjacent).dist = graph.get(graph.get(adjacent).parent).dist + graph.get(source).getDistTo(adjacent);
+
+        }
+        System.out.println("dist: "+dist);
+        System.out.println("node: "+node);
+
+        return dijkstra(graph, node, destination);
+    }
+    
+    static HashMap<String, Node> nodes = new HashMap<>();
     
     public static void main(String[] args) {
+        nodes.put("A", new Node(new String[]{"B", "F"}, new Integer[]{8, 10}));
+        nodes.put("B", new Node(new String[]{"C", "E"}, new Integer[]{4, 10}));
+        nodes.put("C", new Node(new String[]{"D"}, new Integer[]{3}));
+        nodes.put("D", new Node(new String[]{"E", "F"}, new Integer[]{25, 18}));
+        nodes.put("E", new Node(new String[]{"D", "G"}, new Integer[]{9, 7}));
+        nodes.put("F", new Node(new String[]{"A", "B", "C", "E"}, new Integer[]{5, 7, 3, 2}));
+        nodes.put("G", new Node(new String[]{"D", "H"}, new Integer[]{2, 3}));
+        nodes.put("H", new Node(new String[]{"A", "B"}, new Integer[]{4, 9}));
+        
+        dijkstra(nodes, "G", "F");
 
+        System.out.println(nodes.get("F").parent);
+
+
+//        for ( String node : nodes.keySet()) {
+//            System.out.println(node+":");
+//            for (String adjacent : nodes.get(node).adjacents.keySet()) {
+//                System.out.println("    "+adjacent + " "+nodes.get(node).adjacents.get(adjacent));
+//            }
+//        }
+        
     }
 }
--- Excercises/src/dijkstra/Node.java
+++ Excercises/src/dijkstra/Node.java
@@ -1,8 +1,32 @@
 package dijkstra;
 
+import java.util.HashMap;
+
 /**
  * Created by 95028672 on 2/3/2015.
  */
 public class Node {
 
+    public HashMap<String, Integer> adjacents;
+    public String parent;
+    public int dist;
+
+    public Node() {
+        dist = Integer.MAX_VALUE;
+        parent = null;
+    }
+    
+    public Node(String[] nodes, Integer[] dists) {
+        adjacents = new HashMap<>();
+        for (int i = 0; i < ((nodes.length < dists.length) ? nodes.length : dists.length); ++i) {
+            adjacents.put(nodes[i], dists[i]);
+        }
+        dist = Integer.MAX_VALUE;
+        parent = null;
+    }
+    
+    public int getDistTo(String adjacentNode) {
+        return adjacents.get(adjacentNode);
+        
+    }
 }
